a4215f7eb5b454c7227b0cc4429da215fc94ac5e
markpricebond <mark.price@bond.fi>
2024-06-13 20:24:22 +0200

    change key for env variable

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,24 +3,27 @@ import { NextResponse, NextRequest } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   const payload = await req.text();
-  const response = JSON.parse(payload);
+  const res = JSON.parse(payload);
 
   const sig = req.headers.get("Stripe-Signature");
 
-  const dateTime = new Date(response?.created * 1000).toLocaleDateString();
-  const timeString = new Date(response?.created * 1000).toLocaleTimeString();
+  const dateTime = new Date(res?.created * 1000).toLocaleDateString();
+  const timeString = new Date(res?.created * 1000).toLocaleTimeString();
 
   try {
     let event = stripe.webhooks.constructEvent(
       payload,
       sig!,
-      process.env.TESTING_STRIPE_WEBHOOK_SECRET!
+      process.env.STRIPE_WEBHOOK_SECRET!
     );
-    console.log(
-      "here we can do some stuff like add subscribed=true subscription_slot=0 to the user"
-    );
+
+    // const response: any = await updateUserSubscription(
+    //   // insert stuff which updates a stored user's (probably in Supabase) information
+    // );
+    console.log("Event: ", event.type);
+
     return NextResponse.json({ status: "success", event: event.type });
   } catch (error) {
     console.log(error);
